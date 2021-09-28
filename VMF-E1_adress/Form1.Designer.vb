2f1aba126942c0ff7ca4bc216bc489a17cd8c41e
ufrs12 <ufrs12@yandex.ru>
2021-09-29 01:35:53 +0300

    -

--- VMF-E1_adress/Form1.Designer.vb
+++ VMF-E1_adress/Form1.Designer.vb
@@ -31,6 +31,25 @@ Partial Class Form1
         Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ComboBox1
@@ -38,7 +57,7 @@ Partial Class Form1
         Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(12, 25)
+        Me.ComboBox1.Location = New System.Drawing.Point(375, 20)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(76, 21)
         Me.ComboBox1.TabIndex = 1
@@ -46,7 +65,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Location = New System.Drawing.Point(457, 23)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(32, 13)
         Me.Label1.TabIndex = 2
@@ -65,15 +84,15 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 180)
+        Me.TextBox1.Location = New System.Drawing.Point(15, 335)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(649, 179)
+        Me.TextBox1.Size = New System.Drawing.Size(649, 117)
         Me.TextBox1.TabIndex = 4
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(266, 385)
+        Me.Button2.Location = New System.Drawing.Point(247, 471)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 6
@@ -88,27 +107,185 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(118, 75)
+        Me.Button3.Location = New System.Drawing.Point(33, 75)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(106, 36)
         Me.Button3.TabIndex = 7
-        Me.Button3.Text = "Пказать адрес на панеле"
+        Me.Button3.Text = "Показать адрес на панеле"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(235, 75)
+        Me.Button4.Location = New System.Drawing.Point(150, 75)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(106, 36)
         Me.Button4.TabIndex = 7
         Me.Button4.Text = "Вернуться в нормальный режим"
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(367, 183)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(85, 41)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "Сканировать"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(458, 197)
+        Me.ProgressBar1.Maximum = 50
+        Me.ProgressBar1.Minimum = 1
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(104, 10)
+        Me.ProgressBar1.TabIndex = 9
+        Me.ProgressBar1.Value = 1
+        Me.ProgressBar1.Visible = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(568, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(96, 212)
+        Me.ListBox1.TabIndex = 10
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 12)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(35, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Один адрес"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(355, 63)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton1.TabIndex = 13
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(71, 12)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(35, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(112, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "до"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(355, 105)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton2.TabIndex = 13
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(137, 12)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(35, 20)
+        Me.TextBox4.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(7, 16)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Адреса от"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Enabled = False
+        Me.GroupBox2.Location = New System.Drawing.Point(375, 90)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(187, 38)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Enabled = False
+        Me.GroupBox3.Location = New System.Drawing.Point(375, 134)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(187, 38)
+        Me.GroupBox3.TabIndex = 14
+        Me.GroupBox3.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(26, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Все"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(355, 150)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton3.TabIndex = 15
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Enabled = False
+        Me.GroupBox1.Location = New System.Drawing.Point(375, 47)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(187, 38)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(673, 435)
+        Me.ClientSize = New System.Drawing.Size(673, 551)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label1)
@@ -118,6 +295,12 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "VMF-E1 Adress"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -131,4 +314,20 @@ Partial Class Form1
     Friend WithEvents SerialPort1 As IO.Ports.SerialPort
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
--- VMF-E1_adress/Form1.vb
+++ VMF-E1_adress/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO.Ports
+Imports EasyModbus
 Public Class Form1
-
+    Dim modbusClient As ModbusClient
     Private Sub ComboBox1_Enter(sender As Object, e As EventArgs) Handles ComboBox1.Enter
         ComboBox1.Items.Clear()
         For Each sp As String In My.Computer.Ports.SerialPortNames
@@ -8,16 +9,16 @@ Public Class Form1
         Next
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
         If Button1.Text = "Открыть порт" Then
             Try
-                SerialPort1.PortName = ComboBox1.SelectedItem
-                SerialPort1.BaudRate = 19200
-                SerialPort1.Parity = 0
-                SerialPort1.DataBits = 8
-                SerialPort1.StopBits = StopBits.Two
-                SerialPort1.Handshake = 0
-                SerialPort1.Open()
+
+                'SerialPort1.PortName = ComboBox1.SelectedItem
+                'SerialPort1.BaudRate = 19200
+                'SerialPort1.Parity = 0
+                'SerialPort1.DataBits = 8
+                'SerialPort1.StopBits = StopBits.Two
+                'SerialPort1.Handshake = 0
+                'SerialPort1.Open()
                 Button1.Text = "Освободить порт"
                 Button1.BackColor = Color.LightGreen
                 ComboBox1.Enabled = False
@@ -41,16 +42,83 @@ Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         TextBox1.Text = SerialPort1.StopBits.ToString & SerialPort1.BaudRate.ToString & SerialPort1.Parity.ToString & SerialPort1.DataBits.ToString & SerialPort1.Handshake.ToString
     End Sub
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         'SerialPort1.Write("1710000D000102000440EE")
-        Dim bytes() As Byte = System.Text.Encoding.UTF8.GetBytes("1710000D000102000440EE")
+        Dim bytes() As Byte = FromHex("17 10 00 0D 00 01 02 00 04 40 EE")
         SerialPort1.Write(bytes, 0, bytes.Length)
     End Sub
-
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         'SerialPort1.Write("1710000D0001020000412D")
-        Dim bytes() As Byte = System.Text.Encoding.UTF8.GetBytes("1710000D0001020000412D")
+        Dim bytes() As Byte = FromHex("17 10 00 0D 00 01 02 00 00 41 2D")
         SerialPort1.Write(bytes, 0, bytes.Length)
+    End Sub
+    Public Shared Function FromHex(ByVal hex As String) As Byte()
+        hex = hex.Replace(" ", "")
+        Dim raw((hex.Length \ 2) - 1) As Byte
+        For i As Integer = 0 To raw.Length - 1
+            raw(i) = Convert.ToByte(hex.Substring(i * 2, 2), 16)
+        Next i
+        Return raw
+    End Function
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Button1.Text = "Сканировать" Then
+            Dim h, l As Integer
+            Try
+                Button5.Text = "Остановить"
+                For i As Integer = 1 To 50
+                    ProgressBar1.Visible = True
+                    ProgressBar1.Value = i
+                    Try
+                        modbusClient = New ModbusClient(ComboBox1.SelectedItem) With {
+                        .UnitIdentifier = i,
+                        .Parity = 0,
+                        .StopBits = StopBits.Two,
+                        .ConnectionTimeout = 200
+                        }
+                        modbusClient.Connect()
+                        Dim ReadValues() As Integer = modbusClient.ReadHoldingRegisters(40021, 1)
+                        ListBox1.Items.Add(ReadValues(0))
+                        modbusClient.Disconnect()
+                    Catch ex As Exception
+                        'MessageBox.Show(ex.Message)
+                        modbusClient.Disconnect()
+                    End Try
+                Next
+                ProgressBar1.Visible = False
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            Button1.Text = "Сканировать"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RadioButton1.Checked = True
+        TextBox3.Text = 1
+        TextBox4.Text = 50
+        TextBox1.TextAlign = HorizontalAlignment.Center
+        TextBox2.TextAlign = HorizontalAlignment.Center
+        TextBox3.TextAlign = HorizontalAlignment.Center
+        TextBox4.TextAlign = HorizontalAlignment.Center
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        GroupBox1.Enabled = True
+        GroupBox2.Enabled = False
+        GroupBox3.Enabled = False
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        GroupBox1.Enabled = False
+        GroupBox2.Enabled = True
+        GroupBox3.Enabled = False
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        GroupBox1.Enabled = False
+        GroupBox2.Enabled = False
+        GroupBox3.Enabled = True
     End Sub
 End Class
