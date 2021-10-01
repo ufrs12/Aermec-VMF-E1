34fd9b84130f2352ac19c060bc8201014f03d9fe
ufrs12 <ufrs12@yandex.ru>
2021-10-01 12:53:48 +0300

    1
    1

---
+++ VMF-E1-Modbus/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿
+namespace ModbusCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label7 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.RadioButton3 = new System.Windows.Forms.RadioButton();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.TextBox4 = new System.Windows.Forms.TextBox();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.RadioButton2 = new System.Windows.Forms.RadioButton();
+            this.RadioButton1 = new System.Windows.Forms.RadioButton();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GroupBox1.SuspendLayout();
+            this.GroupBox3.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label7.ForeColor = System.Drawing.Color.Red;
+            this.Label7.Location = new System.Drawing.Point(50, 230);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(0, 13);
+            this.Label7.TabIndex = 30;
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(91, 214);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(0, 13);
+            this.Label6.TabIndex = 29;
+            // 
+            // RadioButton3
+            // 
+            this.RadioButton3.AutoSize = true;
+            this.RadioButton3.Location = new System.Drawing.Point(9, 158);
+            this.RadioButton3.Name = "RadioButton3";
+            this.RadioButton3.Size = new System.Drawing.Size(14, 13);
+            this.RadioButton3.TabIndex = 28;
+            this.RadioButton3.TabStop = true;
+            this.RadioButton3.UseVisualStyleBackColor = true;
+            this.RadioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.TextBox2);
+            this.GroupBox1.Controls.Add(this.Label2);
+            this.GroupBox1.Enabled = false;
+            this.GroupBox1.Location = new System.Drawing.Point(29, 55);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(187, 38);
+            this.GroupBox1.TabIndex = 25;
+            this.GroupBox1.TabStop = false;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(6, 12);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(35, 20);
+            this.TextBox2.TabIndex = 11;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(47, 16);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(66, 13);
+            this.Label2.TabIndex = 12;
+            this.Label2.Text = "Один адрес";
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.Label5);
+            this.GroupBox3.Enabled = false;
+            this.GroupBox3.Location = new System.Drawing.Point(29, 142);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(187, 38);
+            this.GroupBox3.TabIndex = 26;
+            this.GroupBox3.TabStop = false;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(6, 16);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(26, 13);
+            this.Label5.TabIndex = 12;
+            this.Label5.Text = "Все";
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.Label4);
+            this.GroupBox2.Controls.Add(this.Label3);
+            this.GroupBox2.Controls.Add(this.TextBox4);
+            this.GroupBox2.Controls.Add(this.TextBox3);
+            this.GroupBox2.Enabled = false;
+            this.GroupBox2.Location = new System.Drawing.Point(29, 98);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(187, 38);
+            this.GroupBox2.TabIndex = 27;
+            this.GroupBox2.TabStop = false;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(7, 16);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(58, 13);
+            this.Label4.TabIndex = 12;
+            this.Label4.Text = "Адреса от";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(112, 16);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(19, 13);
+            this.Label3.TabIndex = 12;
+            this.Label3.Text = "до";
+            // 
+            // TextBox4
+            // 
+            this.TextBox4.Location = new System.Drawing.Point(137, 12);
+            this.TextBox4.Name = "TextBox4";
+            this.TextBox4.Size = new System.Drawing.Size(35, 20);
+            this.TextBox4.TabIndex = 11;
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(71, 12);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(35, 20);
+            this.TextBox3.TabIndex = 11;
+            // 
+            // RadioButton2
+            // 
+            this.RadioButton2.AutoSize = true;
+            this.RadioButton2.Location = new System.Drawing.Point(9, 113);
+            this.RadioButton2.Name = "RadioButton2";
+            this.RadioButton2.Size = new System.Drawing.Size(14, 13);
+            this.RadioButton2.TabIndex = 23;
+            this.RadioButton2.TabStop = true;
+            this.RadioButton2.UseVisualStyleBackColor = true;
+            this.RadioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // RadioButton1
+            // 
+            this.RadioButton1.AutoSize = true;
+            this.RadioButton1.Location = new System.Drawing.Point(9, 71);
+            this.RadioButton1.Name = "RadioButton1";
+            this.RadioButton1.Size = new System.Drawing.Size(14, 13);
+            this.RadioButton1.TabIndex = 24;
+            this.RadioButton1.TabStop = true;
+            this.RadioButton1.UseVisualStyleBackColor = true;
+            this.RadioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(222, 15);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(108, 251);
+            this.listBox2.TabIndex = 22;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(97, 194);
+            this.ProgressBar1.Maximum = 50;
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(117, 10);
+            this.ProgressBar1.TabIndex = 21;
+            this.ProgressBar1.Visible = false;
+            // 
+            // Button5
+            // 
+            this.Button5.Location = new System.Drawing.Point(9, 186);
+            this.Button5.Name = "Button5";
+            this.Button5.Size = new System.Drawing.Size(82, 41);
+            this.Button5.TabIndex = 20;
+            this.Button5.Text = "Сканировать";
+            this.Button5.UseVisualStyleBackColor = true;
+            this.Button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(111, 31);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(32, 13);
+            this.Label1.TabIndex = 19;
+            this.Label1.Text = "Порт";
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Location = new System.Drawing.Point(29, 28);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(76, 21);
+            this.ComboBox1.TabIndex = 18;
+            this.ComboBox1.Enter += new System.EventHandler(this.ComboBox1_Enter);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 36);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Начать опрос";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox2);
+            this.groupBox4.Controls.Add(this.ComboBox1);
+            this.groupBox4.Controls.Add(this.Label7);
+            this.groupBox4.Controls.Add(this.Label1);
+            this.groupBox4.Controls.Add(this.Label6);
+            this.groupBox4.Controls.Add(this.Button5);
+            this.groupBox4.Controls.Add(this.RadioButton3);
+            this.groupBox4.Controls.Add(this.ProgressBar1);
+            this.groupBox4.Controls.Add(this.GroupBox1);
+            this.groupBox4.Controls.Add(this.RadioButton1);
+            this.groupBox4.Controls.Add(this.GroupBox3);
+            this.groupBox4.Controls.Add(this.RadioButton2);
+            this.groupBox4.Controls.Add(this.GroupBox2);
+            this.groupBox4.Location = new System.Drawing.Point(12, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(341, 274);
+            this.groupBox4.TabIndex = 32;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Инициализация";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(111, 15);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(471, 443);
+            this.dataGridView1.TabIndex = 33;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.dataGridView1);
+            this.groupBox5.Controls.Add(this.button1);
+            this.groupBox5.Enabled = false;
+            this.groupBox5.Location = new System.Drawing.Point(359, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(588, 465);
+            this.groupBox5.TabIndex = 34;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Параметры";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Параметр";
+            this.Column1.MinimumWidth = 230;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.Width = 230;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Значение";
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.Width = 140;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "В регистре";
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 483);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "Form1";
+            this.Text = "VMF-E1-Modbus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.GroupBox3.ResumeLayout(false);
+            this.GroupBox3.PerformLayout();
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.RadioButton RadioButton3;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox TextBox4;
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.RadioButton RadioButton2;
+        internal System.Windows.Forms.RadioButton RadioButton1;
+        internal System.Windows.Forms.ListBox listBox2;
+        internal System.Windows.Forms.ProgressBar ProgressBar1;
+        internal System.Windows.Forms.Button Button5;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.ComboBox ComboBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ VMF-E1-Modbus/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using EasyModbus;
+using System.IO.Ports;
+namespace ModbusCS
+{
+    public partial class Form1 : Form
+    {
+        int lo;
+        int hi;
+        int kol;
+        private ModbusClient modbusClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            RadioButton1.Checked = true;
+            TextBox3.Text = "1";
+            TextBox4.Text = "50";
+            TextBox2.TextAlign = HorizontalAlignment.Center;
+            TextBox3.TextAlign = HorizontalAlignment.Center;
+            TextBox4.TextAlign = HorizontalAlignment.Center;
+            dataGridView1.Rows.Add("Температура воздуха на фанкойле", "", "");
+            dataGridView1.Rows.Add("Температура воздуха на панеле", "", "");
+            dataGridView1.Rows.Add("Температура теплоносителя (хладагента)", "", "");
+            dataGridView1.Rows.Add("Температура дополнительного датчика", "", "");
+            dataGridView1.Rows.Add("Действующая уставка температуры", "", "");
+            dataGridView1.Rows.Add("Уставка скорости вентилятора", "", "");
+            dataGridView1.Rows.Add("Реальная скорость вентилятора", "", "");
+            dataGridView1.Rows.Add("Уставка запуска вентилятора", "", "");
+            dataGridView1.Rows.Add("Код аварии", "", "");
+            dataGridView1.Rows.Add("Положение DIP-переключателей", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("Манипуляции с адресом", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("Уставка полученная по удаленке", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("", "", "");
+            dataGridView1.Rows.Add("Адрес", "", "");
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+        private void ComboBox1_Enter(object sender, EventArgs e)
+        {
+            ComboBox1.Items.Clear();
+            foreach (String sp in SerialPort.GetPortNames())
+            {
+                ComboBox1.Items.Add(sp);
+            }
+        }
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            GroupBox1.Enabled = true;
+            GroupBox2.Enabled = false;
+            GroupBox3.Enabled = false;
+        }
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            GroupBox1.Enabled = false;
+            GroupBox2.Enabled = true;
+            GroupBox3.Enabled = false;
+        }
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            GroupBox1.Enabled = false;
+            GroupBox2.Enabled = false;
+            GroupBox3.Enabled = true;
+        }
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (backgroundWorker1.IsBusy != true)
+            {
+                listBox2.Items.Clear();
+                Button5.Text = "Остановить";
+                if (RadioButton1.Checked == true)
+                {
+                    hi = Convert.ToInt32(TextBox2.Text);
+                    lo = Convert.ToInt32(TextBox2.Text);
+                }
+                else if (RadioButton2.Checked == true)
+                {
+                    lo = Convert.ToInt32(TextBox3.Text);
+                    hi = Convert.ToInt32(TextBox4.Text);
+                }
+                else if (RadioButton3.Checked == true)
+                {
+                    lo = 1;
+                    hi = 247;
+                }
+                else
+                {
+                    hi = 0;
+                    lo = 0;
+                }
+                ProgressBar1.Value = 0;
+                ProgressBar1.Visible = true;
+                ProgressBar1.Maximum = hi - lo + 1;
+                backgroundWorker1.RunWorkerAsync();
+                kol = 0;
+            }
+            else
+            {
+                backgroundWorker1.CancelAsync();
+                Button5.Text = "Сканировать";
+                Label7.Text = "";
+                Label6.Text = "";
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Button5.Text = "Сканировать";
+            }
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            worker.WorkerSupportsCancellation = true;
+            for (int i = lo; i <= hi; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        modbusClient = new ModbusClient(Convert.ToString(ComboBox1.SelectedItem))
+                        {
+                            UnitIdentifier = Convert.ToByte(i),
+                            Baudrate = 19200,
+                            Parity = System.IO.Ports.Parity.None,
+                            StopBits = System.IO.Ports.StopBits.Two,
+                            ConnectionTimeout = 200
+                        };
+                        modbusClient.Connect();
+                        int[] adr = modbusClient.ReadHoldingRegisters(20, 1);
+                        modbusClient.Disconnect();
+                        if (adr[0] != 0)
+                        {
+                            listBox2.Items.Add(adr[0]);
+                        }
+                        kol += 1;
+                        ProgressBar1.Value = kol;
+                    }
+                    catch (Exception ex)
+                    {
+                        if (modbusClient != null)
+                        {
+                            modbusClient.Disconnect();
+                        }
+                        MessageBox.Show(ex.Message);
+                        break;
+                    }
+                }
+            }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Button5.Text = "Сканировать";
+            ProgressBar1.Visible = false;
+            Label6.Text = "Опрошено адресов: " + Convert.ToString(kol) + ".";
+            if (kol == 0)
+            {
+                Label7.Text = "Проверьте параметры";
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            groupBox5.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox4.Enabled = false;
+        }
+    }
+}
